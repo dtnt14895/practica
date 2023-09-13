54ad9317475feae3bb72ab568a7810a84821a8b9
Pool Ramos <poolramos.unmsm.edu.pe>
2023-09-13 12:57:05 -0500

    cambios footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,56 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div className='footer-menu'>
+          <img src={footerLogo} alt='sunnyside-logo' />
+
+          <div className='footer-links'>
+            <ul>
+              <li>
+                <a href='#' className='footer-link'>
+                  About
+                </a>
+              </li>
+              <li>
+                <a href='#' className='footer-link'>
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href='#' className='footer-link'>
+                  Projects
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='footer-socials'>
+            <ul>
+              <li>
+                <a href='facebook.com' className='footer-social-icons'>
+                  <img src={facebook} alt='facebook' />
+                </a>
+              </li>
+              <li>
+                <a href='instagram.com' className='footer-social-icons'>
+                  <img src={instagram} alt='instagram' />
+                </a>
+              </li>
+              <li>
+                <a href='twitter.com' className='footer-social-icons'>
+                  <img src={twitter} alt='twitter' />
+                </a>
+              </li>
+              <li>
+                <a href='pinterest.com' className='footer-social-icons'>
+                  <img src={pinterest} alt='pinterest' />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
   )
 }
 
